ca2daf41d656577c3d0541e6c59135521490b307
windowsboy111 <wboy111@outlook.com>
2021-08-31 22:22:27 +0800

    Add commands.help and paginator (#11)

--- guilded/ext/commands/__init__.py
+++ guilded/ext/commands/__init__.py
@@ -3,3 +3,4 @@ from .errors import *
 from .cog import Cog, CogMeta
 from .context import Context
 from .core import command, group, Command, Group
+from .help import Paginator, DefaultHelpCommand, HelpCommand, MinimalHelpCommand, _HelpCommandImpl
