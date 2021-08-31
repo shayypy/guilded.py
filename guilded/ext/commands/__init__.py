81a36cfc7880c26040cb06f293f8197282c1646b
windowsboy111 <wboy111@outlook.com>
2021-08-31 19:43:15 +0800

    Add help.py Remove paginator.py

--- guilded/ext/commands/__init__.py
+++ guilded/ext/commands/__init__.py
@@ -2,4 +2,4 @@ from .bot import Bot
 from .errors import *
 from .cog import Cog, CogMeta
 from .core import command, group, Command, Group
-from .paginator import Paginator
+from .help import Paginator, DefaultHelpCommand, HelpCommand, MinimalHelpCommand, _HelpCommandImpl
