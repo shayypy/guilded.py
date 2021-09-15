da259b478f4b2375b4371f7cea11a049c84ba6b5
shay <43248357+shayypy@users.noreply.github.com>
2021-09-14 19:48:10 -0500

    Correct `enums.AllowDMsFrom` and `enums.AllowFriendRequestsFrom`

--- guilded/enums.py
+++ guilded/enums.py
@@ -53,18 +53,18 @@ from enum import Enum
 
 
 class AllowDMsFrom(Enum):
-    friends_and_members: 'friendsAndServerMembers'
-    members: 'friendsAndServerMembers'
-    friends: 'friendsOnly'
-    everyone: 'everyone'
+    friends_and_members = 'friendsAndServerMembers'
+    members = 'friendsAndServerMembers'
+    friends = 'friendsOnly'
+    everyone = 'everyone'
 
     def __str__(self):
         return self.value
 
 class AllowFriendRequestsFrom(Enum):
-    friends_and_members: 'friendsAndServerMembers'
-    members: 'serverMembersOnly'
-    everyone: 'everyone'
+    friends_and_members = 'friendsAndServerMembers'
+    members = 'serverMembersOnly'
+    everyone = 'everyone'
 
     def __str__(self):
         return self.value
