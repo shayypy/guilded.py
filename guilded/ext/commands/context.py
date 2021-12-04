050824566653ef6bbb301470ced85455f218a5d1
ShashankKumarSaxena <69804741+ShashankKumarSaxena@users.noreply.github.com>
2021-12-04 19:16:30 +0530

    Updated context and added docs + type hints

--- guilded/ext/commands/context.py
+++ guilded/ext/commands/context.py
@@ -50,68 +50,90 @@ DEALINGS IN THE SOFTWARE.
 """
 
 import re
+from typing import Union
 
 import guilded.abc
+from guilded.channel import (
+    ChatChannel,
+    DocsChannel,
+    ListChannel,
+    MediaChannel,
+    ForumChannel,
+    DMChannel,
+    AnnouncementChannel,
+)
+from guilded.user import Member
+from team import Team
+from message import ChatMessage
 
 
 class Context(guilded.abc.Messageable):
     def __init__(self, **attrs):
-        self.message = attrs.pop('message', None)
-        self._state = attrs.pop('state', self.message._state)
-        self.bot = attrs.pop('bot', None)
-        self.args = attrs.pop('args', [])
-        self.kwargs = attrs.pop('kwargs', {})
-        self.prefix = attrs.pop('prefix')
-        self.command = attrs.pop('command', None)
-        self.view = attrs.pop('view', None)
-        self.invoked_with = attrs.pop('invoked_with', None)
-        self.invoked_parents = attrs.pop('invoked_parents', [])
-        self.invoked_subcommand = attrs.pop('invoked_subcommand', None)
-        self.subcommand_passed = attrs.pop('subcommand_passed', None)
-        self.command_failed = attrs.pop('command_failed', False)
+        self.message: ChatMessage = attrs.pop("message", None)
+        self._state = attrs.pop("state", self.message._state)
+        self.bot = attrs.pop("bot", None)
+        self.args = attrs.pop("args", [])
+        self.kwargs = attrs.pop("kwargs", {})
+        self.prefix = attrs.pop("prefix")
+        self.command = attrs.pop("command", None)
+        self.view = attrs.pop("view", None)
+        self.invoked_with = attrs.pop("invoked_with", None)
+        self.invoked_parents = attrs.pop("invoked_parents", [])
+        self.invoked_subcommand = attrs.pop("invoked_subcommand", None)
+        self.subcommand_passed = attrs.pop("subcommand_passed", None)
+        self.command_failed = attrs.pop("command_failed", False)
 
-    def __repr__(self):
-        return f'<Context prefix={self.prefix} message={repr(self.message)}>'
+    def __repr__(self) -> str:
+        return f"<Context prefix={self.prefix} message={repr(self.message)}>"
 
     @property
-    def valid(self):
+    def valid(self) -> bool:
         return self.prefix is not None and self.command is not None
 
     @property
     def cog(self):
+        """Get the cog of invoked command"""
         if self.command is None:
             return None
         return self.command.cog
 
     @property
-    def channel(self):
+    def channel(
+        self,
+    ) -> Union[ChatChannel, DocsChannel, ListChannel, MediaChannel, ForumChannel, DMChannel, AnnouncementChannel]:
+        """Returns the channel in which the command has been invoked"""
         return self.message.channel
 
     @property
-    def _channel_id(self):
+    def _channel_id(self) -> int:
+        """Returns the channel id of channel"""
         return self.message.channel_id
 
     @property
-    def team(self):
+    def team(self) -> Team:
+        """Returns the team instance"""
         return self.message.team
 
     @property
-    def guild(self):
+    def guild(self) -> Team:
+        """Returns the team instance"""
         return self.team
 
     @property
-    def author(self):
+    def author(self) -> Member:
+        """Gives the author of the message"""
         return self.message.author
 
     @property
-    def me(self):
+    def me(self) -> Member:
+        """Returns the invoker's or the client/bot's instance"""
         return self.team.me if self.team else self.bot.user
 
     @property
     def clean_prefix(self) -> str:
         """:class:`str`: The cleaned up invoke prefix. i.e. mentions are ``@name`` instead of ``<@id>``."""
         if self.prefix is None:
-            return ''
+            return ""
 
         user = self.me
         # this breaks if the prefix mention is not the bot itself but I
@@ -119,7 +141,7 @@ class Context(guilded.abc.Messageable):
         # for this common use case rather than waste performance for the
         # odd one.
         pattern = re.compile(r"<@!?%s>" % user.id)
-        return pattern.sub("@%s" % user.display_name.replace('\\', r'\\'), self.prefix)
+        return pattern.sub("@%s" % user.display_name.replace("\\", r"\\"), self.prefix)
 
     def reply(self, *content, **kwargs):
         """|coro|
