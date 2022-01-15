190ab2e8afb3d6739c2e8108506809ab1b8ccf5f
shay <43248357+shayypy@users.noreply.github.com>
2022-01-14 22:05:47 -0600

    Include tasks in `setup.py`

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setuptools.setup(
         'Documentation': 'https://guildedpy.readthedocs.io/en/latest/',
         'Issue tracker': 'https://github.com/shayypy/guilded.py/issues',
     },
-    packages=['guilded', 'guilded.ext.commands'],
+    packages=['guilded', 'guilded.ext.commands', 'guilded.ext.tasks'],
     license='MIT',
     classifiers=[
         'Programming Language :: Python :: 3',
